57a5e891afe7c629faa058e44a1817efb0d2e141
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2022-11-24 09:07:51 +0300

    solution Match Anything with Wildcard Period

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
+
+
+// Match Anything with Wildcard Period
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
