d5f8426027f55337d515131d9ed68a6f371d2775
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2022-11-29 14:24:42 +0300

    feat: new kata
    Find the Longest Word in a String
    Return the length of the longest word in the provided sentence.
    
    Your response should be a number.

--- script.js
+++ script.js
@@ -114,6 +114,14 @@ return Object.keys(obj);
 console.log(getArrayOfUsers(users));
 
 
+// Find the Longest Word in a String
+
+function findLongestWordLength(str) {
+  return str.split(' ').sort((a, b) => b.length - a.length)[0].length;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
 
 
 
